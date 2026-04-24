77fff17b874396ee9464db1b919f1d39095724b6
Nand13112004 <nand13112004@gmail.com>
2026-04-24 21:50:09 +0530

    Update favicon and author metadata

--- config/index.ts
+++ config/index.ts
@@ -35,7 +35,7 @@ export const siteConfig: Metadata = {
     "css",
   ] as Array<string>,
   authors: {
-    name: "Sanidhya Kumar Verma",
-    url: "https://github.com/sanidhyy",
+    name: "Nand Delvadiya",
+    url: "https://github.com/Nand13112004",
   },
 } as const;
